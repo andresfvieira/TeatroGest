6eb2be906d67f41328fde6fadaa405a6e73b9318
eleenadm2 <edm0005@alu.medac.es>
2025-04-01 15:50:15 +0200

    estilado

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./*.html", "./html/*.html", "./js/*.js"], // Revisé las rutas
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+};
